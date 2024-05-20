cddbb57a1696453f9c50907a071345006d1f30c7
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-05-20 10:59:20 +0200

    make it wider a bit

--- components/chat-window.tsx
+++ components/chat-window.tsx
@@ -60,7 +60,7 @@ export function ChatWindow(props: {
 
   return (
     <div
-      className={`flex flex-col items-center w-full max-w-lg mx-auto p-4 md:p-8 rounded grow overflow-hidden ${
+      className={`flex flex-col items-center w-full max-w-2xl mx-auto p-4 md:p-8 rounded grow overflow-hidden ${
         messages.length > 0 ? "border" : ""
       }`}
     >
